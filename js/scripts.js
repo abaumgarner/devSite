961d5089d8080134728f7612bfef905af7efc565
abaumgarner <aaron.baumgarner@eagles.ewu.edu>
2015-12-06 17:13:18 -0800

    Dark theme/night mode

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
 /**
  * Created by Aaron on 12/6/2015.
  */
+
+function nightMode() {
+    $('body').addClass('darkbg');
+    $('div.row div.large-12').css('background', '#141414');
+    $('div.row div.medium-12').css('background', '#141414');
+    $('.menu>li:not(.menu-text)>a').css('color', '#F9EE98');
+    $('.menu-text').css('color', '#F9EE98');
+    $('.sectionhead').css('color', '#F9EE98');
+    $('.content').css('color', '#E1E1E1');
+    $('.link a').css('color', '#E1E1E1');
+    $('div.medium-12.columns ul.thumbs li.content').css('color', '#E1E1E1');
+}
 $(document).ready(function() {
-    $('.thumbs').portfolio({
-        cols: 4,
-        transition: 'slideDown'
-    });
+    nightMode();
 });
