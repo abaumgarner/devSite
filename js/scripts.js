22b9b2fbdaa96cc1d1c91d1a67800915ba4fc240
abaumgarner <aaron.baumgarner@eagles.ewu.edu>
2015-12-07 21:07:05 -0800

    Dark theme
    Finished adding the dark theme JS

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@
 
 function nightMode() {
     $('body').addClass('darkbg');
+    $('head').append('<link rel="stylesheet" type="text/css" href="css/dark.css">');
     $('div.row div.large-12').css('background', '#141414');
     $('div.row div.medium-12').css('background', '#141414');
     $('.menu>li:not(.menu-text)>a').css('color', '#F9EE98');
@@ -12,7 +13,12 @@ function nightMode() {
     $('.content').css('color', '#E1E1E1');
     $('.link a').css('color', '#E1E1E1');
     $('div.medium-12.columns ul.thumbs li.content').css('color', '#E1E1E1');
+    $('div.medium-12.columns div.small-12.columns').css('background', 'rgb(20, 20, 20)');
+    $('div.medium-12.columns div.small-12.columns').css( 'color', 'rgb(249, 238, 152)');
+    $('div.medium-12.columns div.small-12.columns.nobg').css('color', '#E1E1E1');
+    $('div.small-6.columns p').css('color', '#E1E1E1');
+    $('div.medium-12.columns div.small-12.columns.desc ul li').css('color', '#E1E1E1');
 }
 $(document).ready(function() {
-    nightMode();
+    //nightMode();
 });
