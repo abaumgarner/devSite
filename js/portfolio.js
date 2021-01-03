35f12c01fc6925bf11f216e57abaedb683b23fea
Aaron Baumgarner <aarbaumg@gmail.com>
2021-01-03 13:05:28 -0800

    Testing magnificPopup

--- js/portfolio.js
+++ js/portfolio.js
@@ -6,8 +6,11 @@ $(document).ready(function() {
         cols: 4,
         transition: 'slideDown'
     });
+    
     $('.close').portfolio({
         cols: 4,
         transition: 'slideUp'
     });
+
+    $("a.btn.btn-primary").magnificPopup({type:'image'});
 });
